35b9aa9adc7337053ed0aa3704fb07816eb1577b
seiya endo <seiya.endo0830@gmail.com>
2021-05-12 16:23:03 +0900

    implement: comment page component

--- pages/comment-page.tsx
+++ pages/comment-page.tsx
@@ -1,9 +1,30 @@
 import Layout from '../components/Layout'
+import useSWR from 'swr'
+import axios from 'axios'
+import Comment from '../components/Comment'
+import { COMMENT } from '../types/Types'
+
+const axiosFetcher = async () => {
+  const result = await axios.get<COMMENT[]>(
+    'https://jsonplaceholder.typicode.com/comments/?_limit=10'
+  )
+  return result.data
+}
 
 const CommentPage: React.FC = () => {
+  // useSWR で client side fetching
+  // options https://swr.vercel.app/docs/options
+  const { data: comments, error } = useSWR('commentsFetch', axiosFetcher)
+
+  if (error) return <span>Error!</span>
+
   return (
     <Layout title="Comment">
-      <p className="text-4xl mb-10">comment page</p>
+      <p className="text-4xl m-10">comment page</p>
+      <ul>
+        {comments &&
+          comments.map(comment => <Comment key={comment.id} {...comment} />)}
+      </ul>
     </Layout>
   )
 }
